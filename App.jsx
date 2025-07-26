8e23ae828dd896a0ffc23e0b7f25d7483b9cd486
Saiesh <srinivas65888@gmail.com>
2025-07-26 22:52:19 +0530

    added ability to guess

--- App.jsx
+++ App.jsx
@@ -1,18 +1,56 @@
 import { languages } from './languages'
 import React from 'react';
+import { clsx } from 'clsx';
 export default function App() {
 
   /**
- * Goal: Build out the main parts of our app
+ * Goal: Allow the user to start guessing the letters
  * 
- * Challenge: 
- * Display the keyboard ⌨️. Use <button>s for each letter
- * since it'll need to be clickable and tab-accessible.
+ * Challenge: Update the keyboard when a letter is right
+ * or wrong.
+ * 
+ * Bonus: use the `clsx` package to easily add conditional 
+ * classNames to the keys of the keyboard. Check the docs 
+ * to learn how to use it 📖
  */
+  const [currentWord, setCurrentWord] = React.useState('react');
+  const wordBlocks = Array.from(currentWord).map((letter, index) => {
+    return (
+      <span
+        className='word-block'
+        key={index}
+      >
+        {letter.toUpperCase()}
+      </span>)
+  });
+
+  const [guessed, setGuessed] = React.useState([]);
+
+  function handleClick(letter) {
+    setGuessed(prevArr => {
+      const prevSet = new Set(prevArr);
+      prevSet.add(letter);
+      return Array.from(prevSet);
+    });
+  }
 
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
   const keyboard = Array.from(alphabet).map(alpha => {
-    return <button className='alphabet'>{alpha.toUpperCase()}</button>
+    return (
+      <button
+        className={clsx(
+          'alphabet',
+          guessed.includes(alpha) &&
+            (currentWord.includes(alpha)
+            ? 'correct'
+            : 'wrong')
+        )}
+        onClick={() => handleClick(alpha)}
+        key={alpha}
+      >
+        {alpha.toUpperCase()}
+      </button>
+    )
   })
 
   const langElements = languages.map(lang => (
@@ -29,16 +67,7 @@ export default function App() {
   )
   );
 
-  const [currentWord, setCurrentWord] = React.useState('react');
-  const wordBlocks = Array.from(currentWord).map((letter, index) => {
-    return (
-      <span
-        className='word-block'
-        key={index}
-      >
-        {letter.toUpperCase()}
-      </span>)
-  });
+
 
   return (
     <main>
